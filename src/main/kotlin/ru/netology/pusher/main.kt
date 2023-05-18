60a82ec682aca4107cc9969c5fa31c151dd6de11
Dmitriy Prishchepa <prishchepadmitriy@gmail.com>
2023-05-18 23:04:36 +0300

    first sender commit

---
+++ src/main/kotlin/ru/netology/pusher/main.kt
@@ -0,0 +1,31 @@
+package ru.netology.pusher
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import java.io.FileInputStream
+
+
+fun main() {
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
+        .build()
+
+    FirebaseApp.initializeApp(options)
+
+    val message = Message.builder()
+        .putData("action", "NEW_POST")
+        .putData("content", """{
+          "postId": 1,
+          "postAuthorName": "Vasiliy",
+          "postContent": "Procuring education on consulted assurance in do. Is sympathize he expression mr no travelling. Preference he he at travelling in resolution. So striking at of to welcomed resolved. Northward by described up household therefore attention. Excellence decisively nay man yet impression for contrasted remarkably. There spoke happy for you are out. Fertile how old address did showing because sitting replied six. Had arose guest visit going off child she new.",
+     
+        }""".trimIndent())
+        .setToken(token)
+        .build()
+
+    FirebaseMessaging.getInstance().send(message)
+}
+
